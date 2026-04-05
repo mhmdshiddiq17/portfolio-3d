6584f1a2ed109eebd2257e8d2c92ae3b1d90cea4
mhmdshiddiq17 <mhmdshiddiq17@gmail.com>
2026-04-05 14:17:07 +0700

    feat: update webpack configuration and enhance build process with Prisma integration

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['three'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('_http_common');
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
